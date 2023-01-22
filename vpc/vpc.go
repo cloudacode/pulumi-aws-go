9070fdf0585a3bbf99f11d6424bdc18dc8116eb2
kcfigaro <kcfigaro@gmail.com>
2023-01-22 22:13:07 +0100

    added the prefix parameter

--- vpc/vpc.go
+++ vpc/vpc.go
@@ -5,14 +5,14 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func VpcRun(ctx *pulumi.Context, ipamID, ipamPoolId string) error {
+func VpcRunIpam(ctx *pulumi.Context, prefixName, ipamID, ipamPoolId string) error {
 	testVpcIpamPool, err := ec2.GetVpcIpamPool(ctx, ipamID, pulumi.ID(ipamPoolId), &ec2.VpcIpamPoolState{
 		AddressFamily: pulumi.String("ipv4"),
 	})
 	if err != nil {
 		return err
 	}
-	_, err = ec2.NewVpc(ctx, "testVpc", &ec2.VpcArgs{
+	_, err = ec2.NewVpc(ctx, prefixName+"-vpc", &ec2.VpcArgs{
 		Ipv4IpamPoolId:    testVpcIpamPool.ID(),
 		Ipv4NetmaskLength: pulumi.Int(28),
 	})
