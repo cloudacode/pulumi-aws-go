fad82395f6ae94f0b960538e4a0765ca3ca27a4f
kcfigaro <kcfigaro@gmail.com>
2023-01-22 21:58:11 +0100

    added vpc module

---
+++ vpc/go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudacode/pulumi-aws-go/vpc
+
+go 1.19
---
+++ vpc/vpc.go
@@ -0,0 +1,23 @@
+package vpc
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func VpcRun(ctx *pulumi.Context, ipamID, ipamPoolId string) error {
+	testVpcIpamPool, err := ec2.GetVpcIpamPool(ctx, ipamID, pulumi.ID(ipamPoolId), &ec2.VpcIpamPoolState{
+		AddressFamily: pulumi.String("ipv4"),
+	})
+	if err != nil {
+		return err
+	}
+	_, err = ec2.NewVpc(ctx, "testVpc", &ec2.VpcArgs{
+		Ipv4IpamPoolId:    testVpcIpamPool.ID(),
+		Ipv4NetmaskLength: pulumi.Int(28),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
