8d0ac7a8353e61a9d49e881565a3a18ce3deee50
kcfigaro <kcfigaro@gmail.com>
2023-01-29 19:01:52 +0100

    refator fargte code

--- aws/fargate.go
+++ aws/fargate.go
@@ -4,16 +4,29 @@ import (
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecs"
 	awsxecs "github.com/pulumi/pulumi-awsx/sdk/go/awsx/ecs"
+	"github.com/pulumi/pulumi-awsx/sdk/go/awsx/lb"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-// https://www.pulumi.com/docs/guides/crosswalk/aws/ecs/#creating-an-ecs-cluster-in-a-vpc
-func FargateRun(ctx *pulumi.Context, vpcId, prefixName string) error {
+// A collection of values returned by FargateRun.
+type GetFargetRunResult struct {
+	ServiceName pulumi.StringOutput
+	Url         pulumi.StringOutput
+}
 
+// New Fargate registers a new resource with the given vpc id, unique name, image url, and exposed port.
+// https://www.pulumi.com/docs/guides/crosswalk/aws/ecs/#creating-an-ecs-cluster-in-a-vpc
+func FargateRun(ctx *pulumi.Context, vpcId, prefixName, imageUrl string, containerPort int) (*GetFargetRunResult, error) {
+
+	var rv GetFargetRunResult
+
+	// Lookup the VPC information
 	vpc, err := ec2.LookupVpc(ctx, &ec2.LookupVpcArgs{Id: &vpcId})
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	// Get Subents from the VPC
 	subnet, err := ec2.GetSubnets(ctx, &ec2.GetSubnetsArgs{Filters: []ec2.GetSubnetsFilter{
 		{
 			Name:   "vpc-id",
@@ -21,15 +34,17 @@ func FargateRun(ctx *pulumi.Context, vpcId, prefixName string) error {
 		},
 	}})
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	// Set a new security groups
 	securityGroup, err := ec2.NewSecurityGroup(ctx, prefixName+"-sg", &ec2.SecurityGroupArgs{
 		VpcId: pulumi.String(vpc.Id),
 		Ingress: ec2.SecurityGroupIngressArray{
 			ec2.SecurityGroupIngressArgs{
 				Protocol: pulumi.String("tcp"),
-				FromPort: pulumi.Int(80),
-				ToPort:   pulumi.Int(80),
+				FromPort: pulumi.Int(containerPort),
+				ToPort:   pulumi.Int(containerPort),
 				CidrBlocks: pulumi.StringArray{
 					pulumi.String("0.0.0.0/0")},
 			},
@@ -46,13 +61,25 @@ func FargateRun(ctx *pulumi.Context, vpcId, prefixName string) error {
 		},
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	// Create a new ECS cluster
 	cluster, err := ecs.NewCluster(ctx, prefixName+"-cluster", nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	_, err = awsxecs.NewFargateService(ctx, prefixName+"-service", &awsxecs.FargateServiceArgs{
+
+	// Create a LB for the cluster
+	lb, err := lb.NewApplicationLoadBalancer(ctx, "lb", &lb.ApplicationLoadBalancerArgs{
+		DefaultTargetGroupPort: pulumi.Int(containerPort),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a Fargate on the ECS Cluster
+	service, err := awsxecs.NewFargateService(ctx, prefixName+"-service", &awsxecs.FargateServiceArgs{
 		Cluster: cluster.Arn,
 		NetworkConfiguration: &ecs.ServiceNetworkConfigurationArgs{
 			AssignPublicIp: pulumi.Bool(true),
@@ -64,19 +91,32 @@ func FargateRun(ctx *pulumi.Context, vpcId, prefixName string) error {
 		DesiredCount: pulumi.Int(1),
 		TaskDefinitionArgs: &awsxecs.FargateServiceTaskDefinitionArgs{
 			Container: &awsxecs.TaskDefinitionContainerDefinitionArgs{
-				Image:     pulumi.String("nginx:latest"),
+				Image:     pulumi.String(imageUrl),
 				Cpu:       pulumi.Int(256),
 				Memory:    pulumi.Int(512),
 				Essential: pulumi.Bool(true),
+				PortMappings: &awsxecs.TaskDefinitionPortMappingArray{
+					&awsxecs.TaskDefinitionPortMappingArgs{
+						ContainerPort: pulumi.Int(containerPort),
+						HostPort:      pulumi.Int(containerPort),
+						TargetGroup:   lb.DefaultTargetGroup,
+					},
+				},
 			},
 		},
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+
+	// Return values
+	rv.ServiceName = service.Service.Name()
+	rv.Url = lb.LoadBalancer.DnsName()
+
+	return &rv, nil
 }
 
+// Format the StringArray from Strings
 func toPulumiStringArray(a []string) pulumi.StringArrayInput {
 	var res []pulumi.StringInput
 	for _, s := range a {
